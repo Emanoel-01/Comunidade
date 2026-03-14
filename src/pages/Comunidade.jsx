7ad11e6028f1a4d098dea2035c9b030b4865188f
Emanoel Silva de Amorim <emanoel.s.amorim@gmail.com>
2026-03-14 07:24:19 +0000

    File changes

--- src/pages/Comunidade.jsx
+++ src/pages/Comunidade.jsx
@@ -17,6 +17,7 @@ export default function Comunidade() {
   const [loginForm, setLoginForm] = useState({ email: '', password: '' });
   const [activeTab, setActiveTab] = useState('feed');
   const [viewingProfileId, setViewingProfileId] = useState(null);
+  const [chatConversation, setChatConversation] = useState(null);
 
   useEffect(() => {
     checkAuth();
